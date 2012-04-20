7a75448aeb53808f9a0272164236942a839ae822
Michael Wu <mikemwu@stanford.edu>
2012-04-19 23:00:34 -0700

    made a real package

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Fireworks RPC',
+      version='0.1',
+      description='Python bindings to Fireworks XML RPC API',
+      author='Michael Wu',
+      author_email='michaelmwu@gmail.com',
+      packages=['fireworks_rpc'],
+     )
